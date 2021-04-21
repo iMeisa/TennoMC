61ff55ffb736431a13f6460bfe269bcdf8c62dd3
iMeisa <iMeisa@github.com>
2021-04-21 10:41:43 +0300

    Added morphics deposit generation

--- src/main/java/com/meisa/tennomc/blocks/Deposits.java
+++ src/main/java/com/meisa/tennomc/blocks/Deposits.java
@@ -8,26 +8,34 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 
 import java.util.function.Supplier;
 
 public class Deposits {
 
-    public static final RegistryObject<Block> FERRITE_DEPOSIT = register("ferrite_deposit",
-            () -> new Block(AbstractBlock.Properties.of(Material.STONE)
-                    .strength(3f)
-                    .sound(SoundType.STONE)));
-
     public static final RegistryObject<Block> ARGON_DEPOSIT = register("argon_deposit",
             () -> new Block(AbstractBlock.Properties.of(Material.METAL)
                     .strength(5f)
                     .sound(SoundType.METAL)));
 
+    public static final RegistryObject<Block> FERRITE_DEPOSIT = register("ferrite_deposit",
+            () -> new Block(AbstractBlock.Properties.of(Material.STONE)
+                    .strength(3f)
+                    .sound(SoundType.STONE)
+                    .harvestTool(ToolType.PICKAXE)));
+
+    public static final RegistryObject<Block> MORPHICS_DEPOSIT = register("morphics_deposit",
+            () -> new Block(AbstractBlock.Properties.of(Material.HEAVY_METAL)
+                    .instabreak()
+                    .sound(SoundType.METAL)));
+
     public static final RegistryObject<Block> RUBEDO_DEPOSIT = register("rubedo_deposit",
             () -> new Block(AbstractBlock.Properties.of(Material.STONE)
                     .strength(3f)
-                    .sound(SoundType.STONE)));
+                    .sound(SoundType.STONE)
+                    .harvestTool(ToolType.PICKAXE)));
 
 
     public static void register () {}

e82f8e3a4ace3ff0b910c8a74002c02563e1673e
iMeisa <iMeisa@github.com>
2021-04-22 22:57:41 +0300

    Added resource descriptions to blueprints

--- src/main/java/com/meisa/tennomc/items/Blueprint.java
+++ src/main/java/com/meisa/tennomc/items/Blueprint.java
@@ -1,8 +1,15 @@
 package com.meisa.tennomc.items;
 
+import com.meisa.tennomc.util.StringRenamer;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.world.World;
 
+import javax.annotation.Nonnull;
 import java.util.List;
 
 public class Blueprint extends Item {
@@ -24,6 +31,22 @@ public class Blueprint extends Item {
         public Resource(ResourceLocation resourceLocation, int amount) {
             this.resourceLocation = resourceLocation;
             this.amount = amount;
+        }
+    }
+
+    @Override
+    public void appendHoverText(@Nonnull ItemStack stack, World world, List<ITextComponent> tooltip, @Nonnull ITooltipFlag flag) {
+        tooltip.add(new StringTextComponent("Resources:"));
+        for (Resource resource : resources) {
+            String resourceName = resource.resourceLocation.getPath();
+            if (resourceName.contains("_")) {
+                resourceName = StringRenamer.snakeToText(resourceName);
+            } else {
+                resourceName = StringRenamer.capitalize(resourceName);
+            }
+            
+            final String requirement = resource.amount + " " + resourceName;
+            tooltip.add(new StringTextComponent(requirement));
         }
     }
 }
---
+++ src/main/java/com/meisa/tennomc/util/StringRenamer.java
@@ -0,0 +1,19 @@
+package com.meisa.tennomc.util;
+
+public class StringRenamer {
+
+    public static String capitalize(String input) {
+        return input.substring(0, 1).toUpperCase() + input.substring(1);
+    }
+
+    public static String snakeToText(String input) {
+        final String[] splitText = input.split("_");
+        StringBuilder outputText = new StringBuilder();
+        for (String word : splitText) {
+            final String upperCaseWord = word.substring(0, 1).toUpperCase() + word.substring(1) + " ";
+            outputText.append(upperCaseWord);
+        }
+
+        return outputText.toString();
+    }
+}
